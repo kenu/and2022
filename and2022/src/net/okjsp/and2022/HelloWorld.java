c1fe9ed86219b0478e5dd3bd1b600a24caab4baa
myohyeon <gyeong.mh@gmail.com>
2012-05-24 20:00:27 +0900

    hello world commit;

---
+++ and2022/src/net/okjsp/and2022/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.okjsp.and2022;
+
+/**
+ * @author user
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
